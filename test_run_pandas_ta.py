e2d8ca7cb2fd43ee99898899be4469b20d08de8e
Ryan Vuz <rvuz1013@gmail.com>
2025-04-08 15:37:16 -0400

    Updated RMI indicator function and added test call to test run file

--- test_run_pandas_ta.py
+++ test_run_pandas_ta.py
@@ -1,6 +1,7 @@
 import pandas_ta as ta
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import yfinance as yf
 
 # Example: Test a moving average function
@@ -13,7 +14,7 @@ dataframe['SMA'] = ta.sma(dataframe['close'], length=3)
 
 print(dataframe)
 
-df = pd.read_csv("data/SPY_D.csv")
+df = pd.read_csv("data/sample.csv")
 df.sort_values(by=['date'], inplace=True)
 
 df["date"] = pd.to_datetime(df["date"])
@@ -48,4 +49,44 @@ figure.add_trace(go.Scatter(x = df.date,
                             name='SMA'
                             ))
 
-figure.show()
+# figure.show()
+
+fig = make_subplots(
+    rows=2, cols=1,
+    shared_xaxes=True,
+    vertical_spacing=0.03,
+    row_heights=[0.7, 0.3],
+    subplot_titles=("Candlestick Chart", "RMI")
+)
+
+# Candlestick chart
+fig.add_trace(go.Candlestick(
+    x=df.index,
+    open=df["open"],
+    high=df["high"],
+    low=df["low"],
+    close=df["close"],
+    name="Price"
+), row=1, col=1)
+
+# RSI plot
+fig.add_trace(go.Scatter(
+    x=df.index,
+    y=df["rmi"],
+    line=dict(color="purple", width=1),
+    name="RMI"
+), row=2, col=1)
+
+# Add overbought/oversold lines
+fig.add_hline(y=70, line=dict(color="red", dash="dash"), row=2, col=1)
+fig.add_hline(y=30, line=dict(color="green", dash="dash"), row=2, col=1)
+
+# Update layout
+fig.update_layout(
+    height=800,
+    showlegend=False,
+    xaxis_rangeslider_visible=False,
+    title="Candlestick with RMI"
+)
+
+fig.show()
