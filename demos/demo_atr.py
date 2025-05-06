40b1b5393fd7589f5d4bd9b56292903e22ede9d2
rvuz1013 <rvuz1013@gmail.com>
2025-05-06 16:23:35 -0400

    Added demo_atr.py to demonstrate issue #768

---
+++ demos/demo_atr.py
@@ -0,0 +1,15 @@
+import pandas_ta as ta
+import pandas as pd
+import plotly.graph_objects as go
+
+df = pd.read_csv("../data/SPY_D.csv")
+df.sort_values(by=['date'], inplace=True)
+df["date"] = pd.to_datetime(df["date"])
+
+df["ATR"] = ta.atr(high=df.high, low=df.low, close=df.close)
+
+df["ATRP"] = ta.atr(high=df.high, low=df.low, close=df.close, percent=True)
+
+df["NATR"] = ta.natr(high=df.high, low=df.low, close=df.close)
+
+print(df)
