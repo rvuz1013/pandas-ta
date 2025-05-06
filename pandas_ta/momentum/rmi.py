c153014e7d1253a0ad5ef6329d6fcc709d5f1396
rvuz1013 <rvuz1013@gmail.com>
2025-05-06 16:22:36 -0400

    Removed TALIB check for rmi.py due to TALIB not containing rmi function

--- pandas_ta/momentum/rmi.py
+++ pandas_ta/momentum/rmi.py
@@ -33,35 +33,30 @@ def rmi(
     scalar = v_scalar(scalar, 100)
     offset = v_offset(offset)
 
-    # Calculate
-    if Imports["talib"] and mode_tal and length > 1:
-        from talib import RMI
-        rmi = RMI(close, length)
-    else:
-        # Calculate changes in momentum based on closing price
-        momentum_change = close.diff(momentum)
+    # Calculate changes in momentum based on closing price
+    momentum_change = close.diff(momentum)
 
-        gain = momentum_change.clip(lower=0)
-        loss = -momentum_change.clip(upper=0)
+    gain = momentum_change.clip(lower=0)
+    loss = -momentum_change.clip(upper=0)
 
-        # Smooth gains and losses using EMA
-        avg_gain = ema(gain, length=length)
-        avg_loss = ema(loss, length=length)
+    # Smooth gains and losses using EMA
+    avg_gain = ema(gain, length=length)
+    avg_loss = ema(loss, length=length)
 
-        # Calculate RMI using modified relative strength equation
-        rs = avg_gain / avg_loss
-        rmi = scalar - (scalar / (1 + rs))
+    # Calculate RMI using modified relative strength equation
+    rs = avg_gain / avg_loss
+    rmi = scalar - (scalar / (1 + rs))
 
-        # Offset
-        if offset != 0:
-            rmi = rmi.shift(offset)
+    # Offset
+    if offset != 0:
+        rmi = rmi.shift(offset)
 
-        # Fill
-        if "fillna" in kwargs:
-            rmi.fillna(kwargs["fillna"], inplace=True)
+    # Fill
+    if "fillna" in kwargs:
+        rmi.fillna(kwargs["fillna"], inplace=True)
 
-        # Name and Category
-        rmi.name = f"RMI_{length}"
-        rmi.category = "momentum"
+    # Name and Category
+    rmi.name = f"RMI_{length}"
+    rmi.category = "momentum"
 
-        return rmi
+    return rmi
