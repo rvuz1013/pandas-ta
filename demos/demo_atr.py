ff18717ecadb7e25c73f1f05b1680d690a85e622
rvuz1013 <rvuz1013@gmail.com>
2025-05-06 16:30:05 -0400

    Added demo_atr.py to demonstrate solving issue #768

--- demos/demo_atr.py
+++ demos/demo_atr.py
@@ -6,10 +6,10 @@ df = pd.read_csv("../data/SPY_D.csv")
 df.sort_values(by=['date'], inplace=True)
 df["date"] = pd.to_datetime(df["date"])
 
-df["ATR"] = ta.atr(high=df.high, low=df.low, close=df.close)
+df["ATR"] = ta.atr(high=df.high, low=df.low, close=df.close, talib=False)
 
-df["ATRP"] = ta.atr(high=df.high, low=df.low, close=df.close, percent=True)
+df["ATRP"] = ta.atr(high=df.high, low=df.low, close=df.close, percent=True, talib=False)
 
-df["NATR"] = ta.natr(high=df.high, low=df.low, close=df.close)
+df["NATR"] = ta.natr(high=df.high, low=df.low, close=df.close, talib=False)
 
 print(df)
